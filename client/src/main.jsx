914af675aa8842836ee017e33620400ee152024e
dannyverap <danny.verap@gmail.com>
2023-09-15 00:56:10 -0500

    connected tu server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import  store  from "./redux/Store.js";
+import store from "./redux/Store.js";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3004";
+axios.defaults.baseURL = "https://api-torre.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
